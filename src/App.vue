83ff20cff67f626c7f8726136baa87a4a178c55a
ivan <ivan.v@allsports.by>
2024-03-08 12:02:09 +0300

    OJ-1 Setup project vith webpack vue3 and scss, load fonts and create base file structure

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>OJJO JEWELRY</h1>
+</template>
+
+<script>
+
+import { defineComponent } from 'vue'; 
+
+export default defineComponent({
+    name: 'App'
+});
+
+</script>
+
+<style lang="scss">
+h1 {
+    color: aqua;
+}
+</style>
