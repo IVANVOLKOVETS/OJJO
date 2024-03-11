7d2338cfe82ccd67f0a05a621322997fbcb2118d
ivan <ivan.v@allsports.by>
2024-03-11 10:29:18 +0300

    Fix closing sidebar for mobile devices

--- src/layout/BaseHeader.vue
+++ src/layout/BaseHeader.vue
@@ -55,7 +55,7 @@
         id="toggle-sidebar"
         class="header__menu-button"
         :class="showSidebarClass"
-        @click.stop="toggleSidebar"
+        @click.capture.stop="toggleSidebar"
       >
         <span></span>
         <span></span>
@@ -189,6 +189,7 @@ export default defineComponent({
   }
 
   &__menu-button {
+    touch-action: manipulation;
     position: relative;
     display: flex;
     width: 24px;
